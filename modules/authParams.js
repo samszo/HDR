546bed044245e30769c3140114dbff9577ed7694
samszo <samszon@gmail.com>
2023-08-12 07:28:40 +0200

    ignore auth params

--- modules/authParams.js
+++ modules/authParams.js
@@ -2,4 +2,6 @@ export const pa = {
     'navbar':d3.select('#navbarMain'),
     mail:'api@samszo.org',
     apiOmk:'../omk/api',
+    ident: 'lrLmPipjlVOHaoO5gDZEIHT9I3U3TZtZ',
+    key:'yyaEBHRkIOlu0iQKYNrtmq9QzDM9pBFW',
 }
