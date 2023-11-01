00f8f6538dc8b3dd295b38ebb5265d283d22f373
samszo <samszon@gmail.com>
2023-11-01 10:45:20 +0100

    complexity

--- modules/jdcComplexeDatabase.js
+++ modules/jdcComplexeDatabase.js
@@ -38,7 +38,8 @@ export class jdcComplexeDatabase {
                     {'id':"Actant",'select':false}, 
                     {'id':"Concept",'select':false}, 
                     {'id':"Rapport",'select':false}]
-            , rscT = [];
+            , rscT = [] 
+            ,gGraph, zoom, xAxis, yAxis, gX, gY, grid, gGrid, k;
 
         this.init = function () {
             console.log(me.data);            
@@ -82,7 +83,8 @@ export class jdcComplexeDatabase {
                 .attr("height", height)
                 .attr("style", "max-width: 100%; height: auto;")
                 .style("font", "10px sans-serif");                
-                  
+            k = height / width;
+
             // Declare the x (horizontal position) scale.
             //const x = d3.scaleUtc(d3.extent(me.data, d => d[me.pValX]), [marginLeft, width - marginRight]);
             //const x =d3.scaleLinear(d3.extent(dataMissing, d => d[me.pValX]), [marginLeft, width - marginRight]);
@@ -90,9 +92,72 @@ export class jdcComplexeDatabase {
             // Declare the y (vertical position) scale.
             //const y = d3.scaleLinear([0, d3.max(dataMissing, d => d[me.pValY])], [height - marginBottom, marginTop]);
             y = d3.scaleLog([1, d3.max(dataMissing, d => d[me.pValY])], [height - marginBottom, marginTop]).base(2);
+
+            // Add the x-axis.
+            xAxis = (g, x) => g
+                .attr("transform", `translate(0,${height - marginBottom})`)
+                .call(d3.axisBottom(x).ticks(width / 100).tickSizeOuter(0).tickFormat(numberFormat))
+                .call(g => g.append("text")
+                    .attr("x", width - marginRight)
+                    .attr("y", marginBottom)
+                    .attr("fill", "currentColor")
+                    .attr("text-anchor", "end")
+                    .text(me.pLibX));
+
+            // Add the y-axis, remove the domain line, add grid lines and a label.
+            yAxis = (g, y) => g
+                .attr("transform", `translate(${marginLeft},0)`)
+                .call(d3.axisLeft(y).ticks(height / 40).tickFormat(numberFormat))
+                .call(g => g.select(".domain").remove())
+                .call(g => g.selectAll(".tick line").clone()
+                    .attr("x2", width - marginLeft - marginRight)
+                    )
+                .call(g => g.append("text")
+                    .attr("x", -marginLeft)
+                    .attr("y", marginTop-10)
+                    .attr("fill", "currentColor")
+                    .attr("text-anchor", "start")
+                    .text(me.pLibY));
+
+            //ajoute la grille
+            grid = (g, x, y) => g
+            .attr("stroke", "currentColor")
+            .attr("stroke-opacity", 0.1)
+            .call(g => g
+              .selectAll(".x")
+              .data(x.ticks(12))
+              .join(
+                enter => enter.append("line").attr("class", "x").attr("y2", height),
+                update => update,
+                exit => exit.remove()
+              )
+                .attr("x1", d => 0.5 + x(d))
+                .attr("x2", d => 0.5 + x(d)))
+            .call(g => g
+              .selectAll(".y")
+              .data(y.ticks(12 * k))
+              .join(
+                enter => enter.append("line").attr("class", "y").attr("x2", width),
+                update => update,
+                exit => exit.remove()
+              )
+                .attr("y1", d => 0.5 + y(d))
+                .attr("y2", d => 0.5 + y(d)));            
             
+            //gestion du zoom
+            //merci beaucoup à https://observablehq.com/@d3/zoomable-scatterplot?collection=@d3/d3-zoom
+            gGraph = svg.append("g")
+                .attr("fill", "none");
+            gGrid = svg.append("g");
+            gX = svg.append("g");
+            gY = svg.append("g");                
+            zoom = d3.zoom()
+                .scaleExtent([0.5, 32])
+                .on("zoom", zoomed);   
+            svg.call(zoom).call(zoom.transform, d3.zoomIdentity);      
+
             //ajoute la complexité des physiques
-            let gPhysique = svg.append("g")
+            let gPhysique = gGraph.append("g")
                 .attr("class", "cpxPhysique")
                 .attr("stroke", "white")
                 .attr("stroke-opacity", strokeOpacity)
@@ -104,7 +169,7 @@ export class jdcComplexeDatabase {
             addSymbolPhysique(gPhysique);
 
             //ajoute la complexité des actants
-            let gActant = svg.append("g")
+            let gActant = gGraph.append("g")
                     .attr("class", "cpxActant")
                     .attr("stroke", "white")
                     .attr("stroke-opacity", strokeOpacity)
@@ -116,7 +181,7 @@ export class jdcComplexeDatabase {
             addSymbolActant(gActant);
 
             //ajoute la complexité des concepts
-            let gconcept = svg.append("g")
+            let gconcept = gGraph.append("g")
                 .attr("class", "cpxConcept")
                 .attr("stroke", "white")
                 .attr("stroke-opacity", strokeOpacity)
@@ -128,7 +193,7 @@ export class jdcComplexeDatabase {
             addSymbolConcept(gconcept);
 
             //ajoute la complexité des rapports
-            let gRapports = svg.append("g")
+            let gRapports = gGraph.append("g")
                 .attr("class", "cpxRapport")
                 .style("stroke-width", symboleSize)
                 .style("fill", "none")
@@ -140,7 +205,7 @@ export class jdcComplexeDatabase {
             addSymbolRapport(gRapports);
 
             //ajoute la complexité des existances
-            let gExis = svg.append("g")
+            let gExis = gGraph.append("g")
                 .attr("class", "cpxExistence")
               .selectAll()
               .data(dataMissing.filter(d=>d.dim=="Existence"))
@@ -153,41 +218,30 @@ export class jdcComplexeDatabase {
                 .on('click',showDetails);
             addSymbolExistence(gExis);
 
-            // Add the x-axis.
-            svg.append("g")
-                .attr("transform", `translate(0,${height - marginBottom})`)
-                .call(d3.axisBottom(x).ticks(width / 100).tickSizeOuter(0).tickFormat(numberFormat))
-                .call(g => g.append("text")
-                    .attr("x", width - marginRight)
-                    .attr("y", marginBottom)
-                    .attr("fill", "currentColor")
-                    .attr("text-anchor", "end")
-                    .text(me.pLibX));
-                
-            // Add the y-axis, remove the domain line, add grid lines and a label.
-            svg.append("g")
-                .attr("transform", `translate(${marginLeft},0)`)
-                .call(d3.axisLeft(y).ticks(height / 40).tickFormat(numberFormat))
-                .call(g => g.select(".domain").remove())
-                .call(g => g.selectAll(".tick line").clone()
-                    .attr("x2", width - marginLeft - marginRight)
-                    .attr("stroke-opacity", 0.1))
-                .call(g => g.append("text")
-                    .attr("x", -marginLeft)
-                    .attr("y", marginTop-10)
-                    .attr("fill", "currentColor")
-                    .attr("text-anchor", "start")
-                    .text(me.pLibY));
-                  
+            //gestion de l'entête
+            let fontSize=12, scBandHead = d3.scaleBand()
+                    .domain(['titre','dim','dim+','rt'])
+                    .range([0, marginTop-30])
+                    .paddingInner(0.2) // edit the inner padding value in [0,1]
+                    .paddingOuter(0.5) // edit the outer padding value in [0,1]
+                    .align(0.5) // edit the align: 0 is aligned left, 0.5 centered, 1 aligned right.
+
+            svg.append("rect")
+                .attr("fill","#032039")
+                .attr("x",0)
+                .attr("y",0)
+                .attr("height", scBandHead.range()[1])
+                .attr('width',width);
+            
             //ajoute le titre
             svg.append("text")
-                .attr("font-size","2em")
+                .attr("font-size",(fontSize*2)+"px")
                 .attr("fill","currentColor")
-                .attr("transform",d=>`translate(${marginLeft},20)`)
+                .attr("transform",d=>`translate(${marginLeft},${scBandHead('titre')+fontSize})`)
                 .text('Complexity of database : '+numberFormat(me.tots.cpx));
 
             //création des légendes de dimensions
-            let heightLeg=32, scBandLegende = d3.scaleBand()
+            let scBandLegende = d3.scaleBand()
                 .domain(dims.map(d=>d.id))
                 .range([marginLeft, width-marginRight])
                 .paddingInner(0.2) // edit the inner padding value in [0,1]
@@ -202,7 +256,7 @@ export class jdcComplexeDatabase {
                   .attr('id',d=>'leg'+d.id)
                   .attr("stroke", "white")
                   .attr("stroke-opacity", strokeOpacity)  
-                  .attr("transform",d=>`translate(${scBandLegende(d.id)},40)`)
+                  .attr("transform",d=>`translate(${scBandLegende(d.id)},${scBandHead('dim')})`)
                   .on('click',showCacheSymbol)
                   .style("cursor","pointer");
             legende.append("rect")
@@ -210,20 +264,21 @@ export class jdcComplexeDatabase {
                   .attr("x",0)
                   .attr("y",6)
                   .attr("width",scBandLegende.bandwidth())
-                  .attr('height',heightLeg)
+                  .attr('height',scBandHead.bandwidth()*2)
                   .attr("fill",d=>d.select ? "green" : "#00ffff00")
-                  .attr("opacity", 0.2);
+                  //.attr("opacity", 0.2)
+                  ;
             legende.append("text")
                 .attr("text-anchor","middle")
                 .attr("font-size","1em")
                 .attr("x",scBandLegende.bandwidth()/2)
                 .attr("fill","currentColor")
                 .text(d=>d.id);
-            addSymbolExistence(d3.select('#legExistence'),`translate(${scBandLegende.bandwidth()/2},24)`);
-            addSymbolPhysique(d3.select('#legPhysique'),`translate(${(scBandLegende.bandwidth()/2)-(symboleSize*3/2)},${(heightLeg/2)-(symboleSize*3/2)})`);
+            addSymbolExistence(d3.select('#legExistence'),`translate(${scBandLegende.bandwidth()/2},26)`);
+            addSymbolPhysique(d3.select('#legPhysique'),`translate(${(scBandLegende.bandwidth()/2)},20)`);
             addSymbolActant(d3.select('#legActant'),`translate(${scBandLegende.bandwidth()/2},24)`);            
             addSymbolConcept(d3.select('#legConcept'),`translate(${scBandLegende.bandwidth()/2},24)`);            
-            addSymbolRapport(d3.select('#legRapport'),`translate(${scBandLegende.bandwidth()/2},24)`);            
+            addSymbolRapport(d3.select('#legRapport'),`translate(${scBandLegende.bandwidth()/2},26)`);            
             //
             showCacheSymbol(false, false);
             //
@@ -252,7 +307,7 @@ export class jdcComplexeDatabase {
                   .attr('id',d=>'leg'+d.id)
                   .attr("stroke", "white")
                   .attr("stroke-opacity", strokeOpacity)  
-                  .attr("transform",d=>`translate(${scBandLegendeRscT(d)},80)`)
+                  .attr("transform",d=>`translate(${scBandLegendeRscT(d)},${scBandHead('rt')})`)
                   .on('click',showCacheRscT)
                   .style("cursor","pointer");
             legendeRscT.append("rect")
@@ -260,18 +315,28 @@ export class jdcComplexeDatabase {
                   .attr("x",0)
                   .attr("y",6)
                   .attr("width",scBandLegendeRscT.bandwidth())
-                  .attr('height',heightLeg)
+                  .attr('height',scBandHead.bandwidth())
                   .attr("fill","green")
-                  .attr("opacity", 0.2);
+                  //.attr("opacity", 0.2)
+                  ;
             legendeRscT.append("text")
                 .attr("text-anchor","middle")
-                .attr("font-size","1em")
+                .attr("font-size",(fontSize)+"px")
                 .attr("x",scBandLegendeRscT.bandwidth()/2)
-                .attr('y',heightLeg-6)
+                .attr('y',scBandHead.bandwidth()/2+fontSize)
                 .attr("fill","currentColor")
                 .text(d=>d.lib);
 
         }
+
+        function zoomed({transform}) {
+            gGraph.attr("transform", transform);
+            const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
+            const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
+            gX.call(xAxis, zx);
+            gY.call(yAxis, zy);
+            gGrid.call(grid, zx, zy);
+        }                
 
         function showDetails(e,d){
             console.log(d);
@@ -281,16 +346,17 @@ export class jdcComplexeDatabase {
         }
 
         function showCacheRscT(e,t){
-            let sltType = [], s = d3.select('#legFond'+t.id);
-            if(s.attr('fill')=="green"){
-                s.attr('fill',"#00ffff00");//met une couleur transparente pour que le clic fonctionne
-                t.select=false;                        
-            }else{
-                s.attr('fill',"green");                    
-                t.select=true;                        
+            if(e){
+                let s = d3.select('#legFond'+t.id);
+                if(s.attr('fill')=="green"){
+                    s.attr('fill',"#00ffff00");//met une couleur transparente pour que le clic fonctionne
+                    t.select=false;                        
+                }else{
+                    s.attr('fill',"green");                    
+                    t.select=true;                        
+                }
             }
-            let q = rscT.filter(t=>t.select).join(' .');
-            d3.select('.'+q).attr('visibility','visible');
+            showCacheFilter()
         }
         function showCacheSymbol(e,d){
             if(e){
@@ -303,9 +369,16 @@ export class jdcComplexeDatabase {
                     s.attr('fill',"green");                    
                 }    
             }
+            showCacheFilter()
+        }
+        function showCacheFilter(){
             dims.forEach(d=>{
-                if(d.select)d3.select('.cpx'+d.id).attr('visibility','visible');
-                else d3.select('.cpx'+d.id).attr('visibility',d=>'hidden');
+                if(d.select){
+                    rscT.forEach(rt=>{
+                        if(rt.select)d3.selectAll('.symbol'+d.id+'.'+rt.id).attr('visibility','visible');
+                        else d3.selectAll('.symbol'+d.id+'.'+rt.id).attr('visibility','hidden');
+                    });
+                }else d3.selectAll('.symbol'+d.id).attr('visibility','hidden');
             }) 
         }
 
@@ -359,7 +432,7 @@ export class jdcComplexeDatabase {
 
         function addSymbolExistence(g,t){
             let s = t ? g.append('g').attr('transform',t):g;
-            s.attr('class',d => d.rt ? 'symbolExitence '+d.rt.map(t=>t.replace(/\\/g,'')).join(' ') : '');
+            s.attr('class',d => d.rt ? 'symbolExistence '+d.rt.map(t=>t.replace(/\\/g,'')).join(' ') : '');
             s.append("rect")
                 .attr("x", -symboleSize*1.5)
                 .attr("y", -symboleSize*6)
